131a0c2f6b811b43fbcc20e3f54839241f7dbcc7
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2020-01-16 13:28:29 -0300

    Mudado API de pesquisa para cdnjs.com

--- js/actions.js
+++ js/actions.js
@@ -84,7 +84,7 @@ $('#search').change(function () {
 	let searchTerm = $('#search').val();
 	if (searchTerm != '') {
 		$.ajax({
-			url: `https://api.jsdelivr.com/v1/jsdelivr/libraries?name=${searchTerm}*&fields=name`,
+			url: `https://api.cdnjs.com/libraries?search=${searchTerm}`,
 			beforeSend: function () {
 				$('#foundResults').hide();
 				$('#searchInfo').html('<div class="text-align-center"><div class="preloader color-blue"></div></div>');
@@ -92,15 +92,10 @@ $('#search').change(function () {
 			},
 			success: function (data) {
 				$('#foundResults ul').html('');
-				if (data.length) {
-					$('#searchInfo').html(`<div class="block-title">${data.length} resultados</div>`);
-					data.sort(function (a, b) {
-						if (a.name > b.name) { return 1; }
-						if (a.name < b.name) { return -1; }
-						return 0;
-					});
+				if (data.results.length) {
+					$('#searchInfo').html(`<div class="block-title">${data.total} resultados</div>`);
 					$('#foundResults').show();
-					data.forEach(function (elem) {
+					data.results.forEach(function (elem) {
 						$('#foundResults ul').append(`<a class="item-content item-link" onclick="mountDetails('${elem.name}')"><div class="item-inner"><div class="item-title">${elem.name}</div></div></a>`);
 					});
 				} else {
@@ -113,76 +108,57 @@ $('#search').change(function () {
 		});
 	} else {
 		$('#foundResults').hide();
-		$('#searchInfo').html('Digite o termo de busca na caixa acima e pressione Pesquisar no seu teclado ou Enter.<br>Dica: você pode utilizar o caractere * como curinga.<br>Obs.: alguns pacotes podem não funcionar pois a API utilizada foi descontinuada.<br><b class="margin-top">Distribuído por <a href="https://www.jsdelivr.com/" class="link external">jsDelivr</a></b>');
+		$('#searchInfo').html('Digite o termo de busca na caixa acima e pressione Pesquisar no seu teclado ou Enter.<br>Dica: você pode utilizar o caractere * como curinga.<br><b class="margin-top">Distribuído por <a href="https://cdnjs.com/" class="link external">cdnjs.com</a></b>');
 	}
 });
 
 function mountDetails(packageName) {
 	app.preloader.show();
 	$.ajax({
-		url: `https://api.jsdelivr.com/v1/jsdelivr/libraries?name=${packageName}&fields=description`,
+		url: `https://api.cdnjs.com/libraries/${packageName}`,
 		success: function (data) {
-			$('#packageDescription').html(data[0].description || '<i>Sem descrição</i>');
+			latestVersion = `${data.version}` || '';
+			$('#packageName').html(`${packageName} (${latestVersion})`);
+			$('#packageDescription').html(data.description || 'Sem descrição');
+			let defaultFileURL = data.filename ? `https://cdnjs.cloudflare.com/ajax/libs/${packageName}/${latestVersion}/${data.filename}` : undefined;
+			let defaultFileHTML;
 
-			$.ajax({
-				url: `https://data.jsdelivr.com/v1/package/npm/${packageName}`,
-				success: function (data) {
-					latestVersion = `@${data.tags.latest}` || '';
-					$('#packageName').html(`${packageName} (${latestVersion})`);
-					$.ajax({
-						url: `https://data.jsdelivr.com/v1/package/npm/${packageName}${latestVersion}/flat`,
-						success: function (data) {
-							let defaultFileURL = data.default ? `https://cdn.jsdelivr.net/npm/${packageName}`:undefined;
-							let defaultFileHTML;
+			if (defaultFileURL) {
+				if (data.filename.endsWith('.js')) { defaultFileHTML = `<script src=\\\'${defaultFileURL}\\\'></script>`; }
+				else if (data.filename.endsWith('.css')) { defaultFileHTML = `<link rel=\\\'stylesheet\\\' href=\\\'${defaultFileURL}\\\'>`; }
+				else { defaultFileHTML = defaultFileURL; }
 
-							if (defaultFileURL) {
-								if (data.default.endsWith('.js')) { defaultFileHTML = `<script src=\\\'${defaultFileURL}\\\'></script>`; }
-								else if (data.default.endsWith('.css')) { defaultFileHTML = `<link rel=\\\'stylesheet\\\' href=\\\'${defaultFileURL}\\\'>`; }
-								else { defaultFileHTML = defaultFileURL; }
+				$('#addToQuickCodes').show();
+			} else {
+				$('#addToQuickCodes').hide();
+				$('#packageDefaultFile ul').html('<li class="item-content"><div class="item-inner"><div class="item-title">Não possui arquivo principal</div></div></li>');
+			}
 
-								$('#addToQuickCodes').show();
-							} else {
-								$('#addToQuickCodes').hide();
-								$('#packageDefaultFile ul').html('<li class="item-content"><div class="item-inner"><div class="item-title">Não possui arquivo principal</div></div></li>');
-							}
+			defaultFileURL && ($('#addToQuickCodes').attr('onclick', `!packages['Pacotes do cdnjs.com'] && (packages['Pacotes do cdnjs.com'] = {}); packages['Pacotes do cdnjs.com']['${packageName}'] = '${defaultFileHTML}'; $.cookie('quickCodes', JSON.stringify(packages)); app.toast.create({ text: 'Adicionado com sucesso', closeTimeout: 7000, closeButton: true }).open()`));
+			defaultFileURL && ($('#packageDefaultFile ul').html(`<li class="item-content item-link" onclick="editorCurrent.insert('${defaultFileHTML}'); app.toast.create({ text: 'Adicionado', closeTimeout: 7000, closeButton: true }).open()"><div class="item-inner"><div class="item-title">${data.filename}</div></div></li>`));
+			$('#packageFiles ul').html('');
+			data.assets[0].files.forEach(function (file) {
+				if (file.endsWith('.js') || file.endsWith('.css')) {
+					let fileURL = `https://cdnjs.cloudflare.com/ajax/libs/${packageName}/${latestVersion}/${file}`;
+					let fileHTML;
 
-							defaultFileURL && ($('#addToQuickCodes').attr('onclick', `!packages['Pacotes do jsDelivr'] && (packages['Pacotes do jsDelivr'] = {}); packages['Pacotes do jsDelivr']['${packageName}'] = '${defaultFileHTML}'; $.cookie('quickCodes', JSON.stringify(packages)); app.toast.create({ text: 'Adicionado com sucesso', closeTimeout: 7000, closeButton: true }).open()`));
-							defaultFileURL && ($('#packageDefaultFile ul').html(`<li class="item-content item-link" onclick="editorCurrent.insert('${defaultFileHTML}'); app.toast.create({ text: 'Adicionado', closeTimeout: 7000, closeButton: true }).open()"><div class="item-inner"><div class="item-title">${data.default}</div></div></li>`));
-							$('#packageFiles ul').html('');
-							data.files.forEach(function (file) {
-								if (file.name.endsWith('.js') || file.name.endsWith('.css')) {
-									let fileURL = `https://cdn.jsdelivr.net/npm/${packageName}${file.name}`;
-									let fileHTML;
+					if (file.endsWith('.js')) { fileHTML = `<script src=\\\'${fileURL}\\\'></script>`; }
+					else if (file.endsWith('.css')) { fileHTML = `<link rel=\\\'stylesheet\\\' href=\\\'${fileURL}\\\'>`; }
+					else { fileHTML = fileURL; }
 
-									if (file.name.endsWith('.js')) { fileHTML = `<script src=\\\'${fileURL}\\\'></script>`; }
-									else if (file.name.endsWith('.css')) { fileHTML = `<link rel=\\\'stylesheet\\\' href=\\\'${fileURL}\\\'>`; }
-									else { fileHTML = fileURL; }
-
-									$('#packageFiles ul').append(`<li class="item-content item-link" onclick="editorCurrent.insert('${fileHTML}'); app.toast.create({ text: 'Adicionado', closeTimeout: 7000, closeButton: true }).open()"><div class="item-inner"><div class="item-title">${file.name}</div></div></li>`);
-								}
-							});
-							app.views.main.router.navigate('/pSearchDetails/');
-							app.preloader.hide();
-						},
-						error: function () {
-							app.preloader.hide();
-							app.toast.create({
-								text: 'Não foi possível encontrar este pacote',
-								closeTimeout: 7000,
-								closeButton: true
-							}).open();
-						}
-					});
-				},
-				error: function () {
-					app.preloader.hide();
-					app.toast.create({
-						text: 'Não foi possível encontrar este pacote',
-						closeTimeout: 7000,
-						closeButton: true
-					}).open();
+					$('#packageFiles ul').append(`<li class="item-content item-link" onclick="editorCurrent.insert('${fileHTML}'); app.toast.create({ text: 'Adicionado', closeTimeout: 7000, closeButton: true }).open()"><div class="item-inner"><div class="item-title">${file}</div></div></li>`);
 				}
 			});
+			app.views.main.router.navigate('/pSearchDetails/');
+			app.preloader.hide();
+		},
+		error: function () {
+			app.preloader.hide();
+			app.toast.create({
+				text: 'Não foi possível encontrar este pacote',
+				closeTimeout: 7000,
+				closeButton: true
+			}).open();
 		}
 	});
 }
